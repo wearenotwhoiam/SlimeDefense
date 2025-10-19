352fdce79ac156248284a0c8ca7cff3c2af894c8
wearenotwhoiam <weareryanmeyers@gmail.com>
2025-10-18 20:04:46 -0700

    Slimer and Camera

--- SlimeDefense/Source/SlimeDefense/Private/Characters/PlayerSlimeCharacter.cpp
+++ SlimeDefense/Source/SlimeDefense/Private/Characters/PlayerSlimeCharacter.cpp
@@ -2,8 +2,30 @@
 
 
 #include "Characters/PlayerSlimeCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/BoxComponent.h"
 
 #include "DebugHelper.h"
+
+APlayerSlimeCharacter::APlayerSlimeCharacter()
+{
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Comp"));
+	BoxComp->SetupAttachment(GetRootComponent());
+	SetRootComponent(BoxComp);
+
+	SlimeMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Skeletal Mesh"));
+	SlimeMesh->SetupAttachment(GetRootComponent());
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
 
 void APlayerSlimeCharacter::BeginPlay()
 {
--- SlimeDefense/Source/SlimeDefense/Public/Characters/PlayerSlimeCharacter.h
+++ SlimeDefense/Source/SlimeDefense/Public/Characters/PlayerSlimeCharacter.h
@@ -14,7 +14,24 @@ class SLIMEDEFENSE_API APlayerSlimeCharacter : public ATowerDefenseBaseCharacter
 {
 	GENERATED_BODY()
 
+public:
+	APlayerSlimeCharacter();
+
 protected:
 	virtual void BeginPlay() override;
-	
+
+#pragma region Components
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* BoxComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character", meta = (AllowPrivateAccess = "true"))
+	class USkeletalMeshComponent* SlimeMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess= "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+#pragma endregion
 };
